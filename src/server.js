910b726f132fdb9d91e3a4e259a5e51cb947683e
hieu.nguyen <hieu.nguyen@jmango360.com>
2022-10-13 11:51:45 +0700

    add bodyParse and enable cors

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
+const cors = require('cors')
 const express = require("express");
 const initWebRoutes = require("./route/web");
 require("dotenv").config();
-
+const bodyParser = require('body-parser');
 let app = express();
-let port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Application listening on port ${port}`);
-});
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb" }, { extended: true }));
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
 initWebRoutes(app);
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Application listening on port ${port}`);
+});
